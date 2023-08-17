05501cd8a48eac99c3554961e8b125d15233d129
chaitananjfwfuiwbufbrui <59876819+chaitananjfwfuiwbufbrui@users.noreply.github.com>
2023-08-17 18:18:07 +0530

    sdfrgthjk

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from django.urls import path,include
 urlpatterns = [
     path('generate-referral-link/', views.generate_referral_link.as_view(), name='generate_referral_link'),
     path('referral/<str:referral_code>/', views.login_with_referral.as_view(), name='login_with_referral'),
+    path('referrals/', views.checkthereferals.as_view(), name='checkthereferals'),
     # Add more URL patterns for your other views here
 ]
